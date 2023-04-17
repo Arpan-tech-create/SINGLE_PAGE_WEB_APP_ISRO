bf9a768a3be37502bbcb616b685a58c2e26008b7
Arpan-tech-create <bhoiarpan123@gmail.com>
2023-04-17 20:35:57 +0530

    'all the chnages'

--- main.py
+++ main.py
@@ -31,12 +31,22 @@ def delete_data(id):
     conn.close()
     return jsonify({'message': 'Row deleted successfully'})
 
+@app.route('/edit_data/<int:id>', methods=['GET', 'POST'])
+def edit_data(id):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
 
+    if request.method == 'POST':
+        data = request.form
+        cur.execute('UPDATE table_name SET column1 = ?, column2 = ? WHERE id = ?', (data['column1'], data['column2'], id))
+        conn.commit()
+        conn.close()
+        return redirect('/')
 
-
-
-
-
+    cur.execute('SELECT * FROM table_name WHERE id = ?', (id,))
+    data = cur.fetchone()
+    conn.close()
+    return render_template('edit.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
